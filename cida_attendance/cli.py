a3a26fb604836e9a40c4e857125e526afbc87a21
Isaac Mercado <mercadoluis095@gmail.com>
2025-03-18 14:30:06 -0400

    Add CLI commands for configuration checking and synchronization

--- cida_attendance/cli.py
+++ cida_attendance/cli.py
@@ -8,7 +8,7 @@ import typer
 from scheduler import Scheduler
 
 from cida_attendance.config import check_config, save_config
-from cida_attendance.tasks import synchronize
+from cida_attendance.tasks import check_db, check_device, synchronize
 
 app = typer.Typer()
 
@@ -77,8 +77,9 @@ def configure(
         name = typer.prompt("Enter the name")
 
     if gui:
-        from cida_attendance.gui import FormWindow
         from PySide6.QtWidgets import QApplication
+
+        from cida_attendance.gui import FormWindow
 
         app = QApplication([])
         FormWindow().show()
@@ -99,6 +100,36 @@ def configure(
     save_config(uri_db, user, password, ip, port, name)
 
     typer.echo("Configuration saved")
+
+
+@app.command()
+def check():
+    if not check_config():
+        typer.echo("Configuration not set up")
+        raise typer.Abort()
+
+    if not check_db():
+        typer.echo("Database not available")
+        raise typer.Abort()
+
+    check_device()
+    typer.echo("Device checked")
+
+
+@app.command()
+def sync():
+    if not check_config():
+        typer.echo("Configuration not set up")
+        raise typer.Abort()
+
+    if not check_db():
+        typer.echo("Database not available")
+        raise typer.Abort()
+
+    if synchronize():
+        typer.echo("Synchronization finished")
+    else:
+        typer.echo("Synchronization failed")
 
 
 if __name__ == "__main__":
