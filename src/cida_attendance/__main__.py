f7d6e4b8ecc3ff3fd4cb088b455e0224e7158677
IsaacMercado <mercadoluis095@gmail.com>
2026-02-24 11:00:14 -0400

    feat: Actualizar .gitignore y mejorar la configuración del proyecto Nuitka

--- src/cida_attendance/__main__.py
+++ src/cida_attendance/__main__.py
@@ -1,10 +1,27 @@
 # nuitka-project: --python-flag=-m
 # nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --windows-console-mode=attach
 # nuitka-project: --include-package-data=cida_attendance
 # nuitka-project: --include-package=cida_attendance.sdk.api.structs
-# nuitka-project: --include-data-files=libs/*.so*=libs/
-# nuitka-project: --include-data-files=libs/HCNetSDKCom/*.so*=libs/HCNetSDKCom/
-# nuitka-project: --include-data-files=libs/*.xml=libs/
+# nuitka-project: --noinclude-qt-translations
+# nuitka-project: --noinclude-qt-plugins=printsupport
+# nuitka-project: --nofollow-import-to=tkinter,unittest
+
+# nuitka-project-if: {OS} == "Linux":
+#   nuitka-project: --include-data-files=libs/*.so*=libs/
+#   nuitka-project: --include-data-files=libs/HCNetSDKCom/*.so*=libs/HCNetSDKCom/
+#   nuitka-project: --include-data-files=libs/*.xml=libs/
+
+# nuitka-project-if: {OS} == "Windows":
+#   nuitka-project: --include-data-files=libs/*.dll=libs/
+#   nuitka-project: --include-data-files=libs/*.lib=libs/
+#   nuitka-project: --include-data-files=libs/*.zip=libs/
+#   nuitka-project: --include-data-files=libs/*.exe=libs/
+#   nuitka-project: --include-data-files=libs/HCNetSDKCom/*.dll=libs/HCNetSDKCom/
+#   nuitka-project: --include-data-files=libs/HCNetSDKCom/*.lib=libs/HCNetSDKCom/
+#   nuitka-project: --include-data-files=libs/ClientDemoDll/*.dll=libs/ClientDemoDll/
+#   nuitka-project: --include-data-files=libs/ClientDemoDll/*.txt=libs/ClientDemoDll/
+
 
 from cida_attendance import cli
 
